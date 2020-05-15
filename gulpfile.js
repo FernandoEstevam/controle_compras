50fc90eb9f502924a3f29244a24cfb8c5b7dc6b6
Fernando Estevam <fernadoestevam23@gmail.com>
2020-05-15 10:18:41 -0300

    Ajustando atualização da pasta image

--- gulpfile.js
+++ gulpfile.js
@@ -16,13 +16,14 @@ const scss_dir = 'app/assets/scss/**/*.scss';
 const css_dir = 'app/assets/css';
 const js = 'app/assets/js/**/*.js';
 const dist = 'dist';
+const image_dir = 'app/assets/image/**/*';
 
 // SCSS
 gulp.task('scss', () => {
   return gulp.src([scss_dir,'!app/assets/scss/variavel/**'])
     .pipe(scss())
     .pipe(gulp.dest(css_dir))
-    .pipe(browsersync.reload({stream: true}));
+    .pipe(browsersync.stream());
 });
 
 // Minificando arquivos
@@ -37,24 +38,31 @@ gulp.task('minifi', () => {
 });
 
 // Browser sync
-gulp.task('browserSync', () => {
+gulp.task('server', () => {
   browsersync.init({
     server: {
       baseDir: 'app'
     }
   });
-  gulp.watch('app/*.html').on("change", browsersync.reload);
-})
-
-// Verifica mudanças no arquivos
-gulp.task('watch', ()=>{
+  
   gulp.watch(scss_dir, gulp.series('scss'));
   gulp.watch(app + '*.html', gulp.series('minifi'));
   gulp.watch(css_dir, gulp.series('minifi'));
   gulp.watch(js, gulp.series('minifi'));
-  // gulp.watch('app/assets/**/*.*', browsersync.reload);
+  gulp.watch(image_dir, gulp.series('image'));
+
+  gulp.watch('app/*.html').on("change", browsersync.reload);
+  gulp.watch('app/assets/css/**/*.css').on("change", browsersync.reload);
+  gulp.watch('app/assets/js/**/*.js').on("change", browsersync.reload);
+  gulp.watch('app/assets/image/**/*').on("change", browsersync.reload);
+})
+
+// Verifica se houve modificação pasta images
+gulp.task('image', ()=>{
+  return gulp.src(image_dir)
+    .pipe(gulp.dest('dist/assets/image'));
 });
 
 
 // Tarefa padrão
-gulp.task('run-task', gulp.series('browserSync','scss','minifi','watch'));
+gulp.task('run-task', gulp.series('scss','minifi','image','server'));
